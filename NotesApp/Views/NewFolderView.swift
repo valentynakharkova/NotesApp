680a217c7cc28f79f85ee52fd37de4996b78eee2
Valentyna Kharkova <gerry29072001@gmail.com>
2026-04-13 12:24:27 +0300

    [Feature] Created NewFolderView

--- NotesApp/Views/NewFolderView.swift
+++ NotesApp/Views/NewFolderView.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewFolderView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var title: String = ""
+    @State private var viewModel: NotesViewModel?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 12) {
+                HStack {
+                    TextField("New Folder", text: $title)
+                    if !title.isEmpty {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundStyle(.gray.opacity(0.8))
+                            .onTapGesture {
+                                title = ""
+                            }
+                    }
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .frame(height: 40)
+                .background {
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(.gray.opacity(0.1))
+                }
+                .padding()
+                Spacer()
+            
+            }
+            .navigationTitle("New Folder")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                    }
+
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        guard !title.isEmpty else { return }
+                            viewModel?.addFolder(name: title)
+                            dismiss()
+                    } label: {
+                        Image(systemName: "checkmark")
+                            .foregroundStyle(.indigo)
+                    }
+                }
+                
+            }
+            .onAppear {
+                viewModel = NotesViewModel(context: context)
+            }
+        }
     }
 }
 
 #Preview {
     NewFolderView()
+        .modelContainer(for: Folder.self, inMemory: true)
 }
