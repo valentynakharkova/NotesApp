00fbac98a2456970b61f36e844169966aac701ca
Valentyna Kharkova <gerry29072001@gmail.com>
2026-04-17 13:54:24 +0300

    [Feature] Added Edit Fouldsers

--- NotesApp/Views/FoldersView.swift
+++ NotesApp/Views/FoldersView.swift
@@ -10,17 +10,17 @@ import SwiftData
 
 struct FoldersView: View {
     @Environment(\.modelContext) private var context
-    @Query(sort: \Folder.dateCreated, order: .forward) private var folders: [Folder]
+    @Query(sort: \Folder.order, order: .forward) private var folders: [Folder]
     
     @State private var viewModel: NotesViewModel?
-    @State private var showNewNote: Bool = false
     @State private var showNewFolder: Bool = false
-
+    @State private var folderToEdit: Folder? = nil
+    @State private var editedName: String = ""
+    
     var body: some View {
         NavigationStack {
             List {
                 //MARK: All Notes Folder
-                Section {
                     NavigationLink {
                         NotesListView()
                     } label: {
@@ -28,16 +28,50 @@ struct FoldersView: View {
                             Text("All Notes")
                         } icon: {
                             Image(systemName: "folder.fill")
-                                .foregroundStyle(.indigo)
+                                .foregroundStyle(.primary)
                         }
 
                     }
-                }
-                ForEach(folders) { folder in
-                    
-                }
+                //MARK: My Folders
+                    ForEach(folders) { folder in
+                        NavigationLink {
+                            NotesListView(folder: folder)
+                        } label: {
+                            Label {
+                                Text(folder.name)
+                            } icon: {
+                                Image(systemName: "folder.fill")
+                                    .foregroundStyle(.primary)
+                            }
+                        }
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive) {
+                                viewModel?.deleteFolder(folder)
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                        .swipeActions(edge: .trailing) {
+                            Button {
+                                folderToEdit = folder
+                                editedName = folder.name
+                            } label: {
+                                Label("Edit", systemImage: "pencil")
+                                    .background(Color.blue)
+                            }
+                        }
+                    }
+                    .onMove { source, destination in
+                        viewModel?.moveFolder(folders: folders, source: source, destination: destination)
+                    }
+//                    .onDelete { IndexSet in
+//                        IndexSet.forEach { index in
+//                            viewModel?.deleteFolder(folders[index])
+//                        }
+//                    }
             }
             .navigationTitle("Folders")
+            //MARK: Toolbar
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
@@ -53,11 +87,24 @@ struct FoldersView: View {
             .sheet(isPresented: $showNewFolder) {
                 NewFolderView()
             }
+            .alert("Edit Folder", isPresented: Binding(
+                get: { folderToEdit != nil },
+                set: { if !$0 { folderToEdit = nil } }
+            )) {
+                TextField("Folder Name", text: $editedName)
+                Button("Save") {
+                    if let folder = folderToEdit {
+                        folderToEdit?.name = editedName
+                        viewModel?.saveFolder(folder)
+                        folderToEdit = nil
+                    }
+                }
+            }
         }
     }
 }
 
 #Preview {
     FoldersView()
-        .modelContainer(for: Note.self, inMemory: true)
+        .modelContainer(for: [Note.self, Folder.self], inMemory: true)
 }
