560266f9f44705f71f112bda9ab5a39f691e8c37
Valentyna Kharkova <gerry29072001@gmail.com>
2026-04-20 13:16:20 +0300

    [Feauture]Created func to delete folders and notes in Recently Deleted folder

--- NotesApp/ViewModels/NotesViewModel.swift
+++ NotesApp/ViewModels/NotesViewModel.swift
@@ -39,9 +39,36 @@ class NotesViewModel {
         saveContext()
     }
     
-    //MARK: Delete Note
-    func deleteNote(_ note: Note) {
+    //MARK: Soft Delete Note
+    func softDeleteNote(_ note: Note) {
+        note.isDeleted = true
+        note.deleteDate = .now
+        note.isPinned = false
+        saveContext()
+    }
+    
+    //MARK: Restore Note
+    func restoreNote(_ note: Note) {
+        note.isDeleted = false
+        note.deleteDate = nil
+        saveContext()
+    }
+        
+    //MARK: Permanently Delete Note
+    func permanentlyDeleteNote(_ note: Note) {
         context.delete(note)
+        saveContext()
+    }
+    
+    //MARK: Cleanup expires Notes older than 30 days
+    func clenupExpiredNotes(notes: [Note]) {
+        let expiredNotes = Calendar.current.date(byAdding: .day, value: -30, to: .now) ?? .now
+        for note in notes {
+            if let deleteDate = note.deleteDate, deleteDate < expiredNotes {
+                context.delete(note)
+            }
+        }
+        saveContext()
     }
     
     //MARK: Save Note
@@ -62,10 +89,42 @@ class NotesViewModel {
         saveContext()
     }
     
-    //MARK: Delete Folder
-    func deleteFolder(_ folder: Folder) {
+    //MARK: Soft Delete Folder
+    func softDeleteFolder(_ folder: Folder) {
+        folder.isDeleted = true
+        folder.deleteDate = .now
+        folder.isPinned = false
+        for note in folder.notes {
+            note.isDeleted = true
+            note.deleteDate = .now
+        }
         context.delete(folder)
+        saveContext()
     }
+    
+    //MARK: Restore Folder
+//    func restoreFolder(_ folder: Folder) {
+//        folder.isDeleted = false
+//        folder.deleteDate = nil
+//        saveContext()
+//    }
+        
+    //MARK: Permanently Delete Folder
+//    func permanentlyDeleteFolder(_ folder: Folder) {
+//        context.delete(folder)
+//        saveContext()
+//    }
+    
+    //MARK: Cleanup expires Folders older than 30 days
+//    func clenupExpiredFolders(folders: [Folder]) {
+//        let expiredFolders = Calendar.current.date(byAdding: .day, value: -30, to: .now) ?? .now
+//        for folder in folders {
+//            if let deleteDate = folder.deleteDate, deleteDate < expiredFolders {
+//                context.delete(folder)
+//            }
+//        }
+//        saveContext()
+//    }
     
     //MARK: Save Folder
     func saveFolder(_ folder: Folder) {
