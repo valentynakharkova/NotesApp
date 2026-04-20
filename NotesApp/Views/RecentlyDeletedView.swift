edbadfc117c3aa50f8af7c762b94e9a745373a4a
Valentyna Kharkova <gerry29072001@gmail.com>
2026-04-20 13:09:57 +0300

    [Feature] Added App Icon

---
+++ NotesApp/Views/RecentlyDeletedView.swift
@@ -0,0 +1,18 @@
+//
+//  RecentlyDeletedView.swift
+//  NotesApp
+//
+//  Created by Valentyna Kharkova on 18.04.2026.
+//
+
+import SwiftUI
+
+struct RecentlyDeletedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentlyDeletedView()
+}
