550370aab4a329f02c36940101a61eb44d2a52a7
Valentyna Kharkova <gerry29072001@gmail.com>
2026-06-03 16:36:01 +0300

    [Feature] Created RichTextEditor+Helpers view

--- NotesApp/Views/Toolbar/RichTextEditor+Helpers.swift
+++ NotesApp/Views/Toolbar/RichTextEditor+Helpers.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+extension RichTextEditor.Coordinator {
+    
+    // MARK: - Checkbox Attachment
+    func makeCheckboxAttributedString(checked: Bool) -> NSMutableAttributedString {
+        let attachment = makeCheckboxAttachment(checked: checked)
+        let result = NSMutableAttributedString(attachment: attachment)
+        result.append(NSAttributedString(
+            string: " ",
+            attributes: [
+                .font: UIFont.systemFont(ofSize: 16),
+                .foregroundColor: UIColor.label
+            ]
+        ))
+        return result
+    }
+    
+    // Create NSTextAttachment with SF Symbol
+    func makeCheckboxAttachment(checked: Bool) -> NSTextAttachment {
+        let attachment = checked ? CheckedAttachment() : UncheckedAttachment()
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .regular)
+            .applying(UIImage.SymbolConfiguration(paletteColors: [checked ? .systemIndigo : .label]))
+        let imageName = checked ? "checkmark.circle.fill" : "circle"
+        attachment.image = UIImage(systemName: imageName, withConfiguration: config)
+        attachment.bounds = CGRect(x: 0, y: -3, width: 22, height: 22)
+        
+        return attachment
+    }
+}
